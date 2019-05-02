7bb150bf66cbe86f73b07ea39e83469787bca607
Raoul Kramer <raoul.kramer@bold-ventures.de>
2019-05-02 22:13:06 +0200

    add dist folder to git

---
+++ dist/lazy-scripts.es.js
@@ -0,0 +1,138 @@
+/*! LazyScripts - v0.0.1 - 2019-05-02
+* https://lazyscripts.raoulkramer.de
+* Copyright (c) 2019 Raoul Kramer; Licensed GNU General Public License v3.0 */
+
+
+function lazyScripts (customOptions = {}) {
+  const options = {
+    lazyScriptSelector: '[data-lazy-script]',
+    lazyScriptsSelector: '[data-lazy-scripts]',
+    ...customOptions,
+  };
+
+  const lazyScriptDataName = hyphensToCamelCase(options.lazyScriptSelector);
+  const lazyScriptsDataName = hyphensToCamelCase(options.lazyScriptsSelector);
+
+  const loadedScripts = [];
+  const lazyScripts = document.querySelectorAll(
+    `${options.lazyScriptSelector}, ${options.lazyScriptsSelector}`
+  );
+
+  
+
+  /**
+   * convert the `querySelectorAll` compatible class options of lazySelectors and
+   * return a string, you can use in `dataset[string]`
+   * @see https://stackoverflow.com/a/6661012
+   * @param {String} string - the text you want to convert
+   */
+  function hyphensToCamelCase(string) {
+    return string.replace('[data-', '').replace(']', '').replace(/-([a-z])/g, g => g[1].toUpperCase());
+  }
+
+  /**
+   * store all `<script src="…"></script>` scripts
+   * to know that they are available and don't need to
+   * be loaded again
+   */
+  function initLoadedScripts() {
+    document.querySelectorAll('script').forEach((script) => {
+      loadedScripts.push(script.src);
+    });
+  }
+
+  /**
+   * create a `<script>` element, add type, defer and src attribute
+   * and append it into the lazyElement
+   * @param {Array} scriptsSrc - Array with full paths to js files
+   * @param {HTMLElement} element - element the script will be append to
+   */
+  function loadScript(scriptsSrc, element) {
+    const scriptSrc = scriptsSrc.shift();
+    if (scriptSrc && loadedScripts.indexOf(scriptSrc) === -1) {
+      const script = document.createElement('script');
+      loadedScripts.push(scriptSrc);
+      script.type = 'text/javascript';
+      script.src = scriptSrc;
+      if (scriptsSrc.length > 0) {
+        script.onload = () => {
+          loadScript(scriptsSrc, element);
+        };
+      }
+      element.appendChild(script);
+    } else if (scriptSrc.length > 0) {
+      loadScript(scriptsSrc, element);
+    }
+  }
+
+  /**
+   * callback from fallback or intersectionObserver method,
+   * to process both data attributes from given element and
+   * load its scripts
+   * @param {HTMLElement} lazyElement - the element which entered the viewport and will be processed
+   */
+  function processElement(lazyElement) {
+    // process single script data attribute
+    if (lazyElement.dataset[lazyScriptDataName]) {
+      loadScript([lazyElement.dataset[lazyScriptDataName]], lazyElement);
+    }
+
+    const scripts = JSON.parse(lazyElement.dataset[lazyScriptsDataName] || '[]');
+    loadScript(scripts, lazyElement);
+  }
+
+  /**
+   * if no IntersectionObserver (and no polyfill) is found,
+   * all scripts are gonna be loaded in a batch
+   */
+  function fallbackScriptLoad() {
+    lazyScripts.forEach(lazyScript => processElement(lazyScript));
+  }
+
+  /**
+   * check every entry if it is `isIntersecting` and load
+   * its scripts.
+   * when scripts are loaded, the element will be removed from
+   * IntersectionObserver
+   * @param {Array} entries - IntersectionObserver entry array
+   * @param {IntersectionObserver} observer - the IntersectionObserver itself
+   */
+  function intersectionCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        processElement(entry.target);
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  /**
+   * setup IntersectionObserver (if available)
+   * or start loading all scripts within a `requestAnimationFrame`
+   * callback - if available
+   */
+  function setup() {
+    initLoadedScripts();
+    if (!window.IntersectionObserver) {
+      if (!window.requestAnimationFrame) {
+        fallbackScriptLoad();
+      } else {
+        window.requestAnimationFrame(() => fallbackScriptLoad());
+      }
+      return;
+    }
+
+    const io = new IntersectionObserver(
+      (entries, observer) => intersectionCallback(entries, observer),
+      options.intersectionObserverOptions,
+    );
+
+    lazyScripts.forEach((lazyScript) => {
+      io.observe(lazyScript);
+    });
+  }
+
+  setup();
+}
+
+export default lazyScripts;
---
+++ dist/lazy-scripts.js
@@ -0,0 +1,191 @@
+/*! LazyScripts - v0.0.1 - 2019-05-02
+* https://lazyscripts.raoulkramer.de
+* Copyright (c) 2019 Raoul Kramer; Licensed GNU General Public License v3.0 */
+
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.LazyScripts = factory());
+}(this, function () { 'use strict';
+
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+
+    return obj;
+  }
+
+  function _objectSpread(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i] != null ? arguments[i] : {};
+      var ownKeys = Object.keys(source);
+
+      if (typeof Object.getOwnPropertySymbols === 'function') {
+        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        }));
+      }
+
+      ownKeys.forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    }
+
+    return target;
+  }
+
+  function lazyScripts () {
+    var customOptions = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    var options = _objectSpread({
+      lazyScriptSelector: '[data-lazy-script]',
+      lazyScriptsSelector: '[data-lazy-scripts]'
+    }, customOptions);
+
+    var lazyScriptDataName = hyphensToCamelCase(options.lazyScriptSelector);
+    var lazyScriptsDataName = hyphensToCamelCase(options.lazyScriptsSelector);
+    var loadedScripts = [];
+    var lazyScripts = document.querySelectorAll("".concat(options.lazyScriptSelector, ", ").concat(options.lazyScriptsSelector));
+    /**
+     * convert the `querySelectorAll` compatible class options of lazySelectors and
+     * return a string, you can use in `dataset[string]`
+     * @see https://stackoverflow.com/a/6661012
+     * @param {String} string - the text you want to convert
+     */
+
+    function hyphensToCamelCase(string) {
+      return string.replace('[data-', '').replace(']', '').replace(/-([a-z])/g, function (g) {
+        return g[1].toUpperCase();
+      });
+    }
+    /**
+     * store all `<script src="…"></script>` scripts
+     * to know that they are available and don't need to
+     * be loaded again
+     */
+
+
+    function initLoadedScripts() {
+      document.querySelectorAll('script').forEach(function (script) {
+        loadedScripts.push(script.src);
+      });
+    }
+    /**
+     * create a `<script>` element, add type, defer and src attribute
+     * and append it into the lazyElement
+     * @param {Array} scriptsSrc - Array with full paths to js files
+     * @param {HTMLElement} element - element the script will be append to
+     */
+
+
+    function loadScript(scriptsSrc, element) {
+      var scriptSrc = scriptsSrc.shift();
+
+      if (scriptSrc && loadedScripts.indexOf(scriptSrc) === -1) {
+        var script = document.createElement('script');
+        loadedScripts.push(scriptSrc);
+        script.type = 'text/javascript';
+        script.src = scriptSrc;
+
+        if (scriptsSrc.length > 0) {
+          script.onload = function () {
+            loadScript(scriptsSrc, element);
+          };
+        }
+
+        element.appendChild(script);
+      } else if (scriptSrc.length > 0) {
+        loadScript(scriptsSrc, element);
+      }
+    }
+    /**
+     * callback from fallback or intersectionObserver method,
+     * to process both data attributes from given element and
+     * load its scripts
+     * @param {HTMLElement} lazyElement - the element which entered the viewport and will be processed
+     */
+
+
+    function processElement(lazyElement) {
+      // process single script data attribute
+      if (lazyElement.dataset[lazyScriptDataName]) {
+        loadScript([lazyElement.dataset[lazyScriptDataName]], lazyElement);
+      }
+
+      var scripts = JSON.parse(lazyElement.dataset[lazyScriptsDataName] || '[]');
+      loadScript(scripts, lazyElement);
+    }
+    /**
+     * if no IntersectionObserver (and no polyfill) is found,
+     * all scripts are gonna be loaded in a batch
+     */
+
+
+    function fallbackScriptLoad() {
+      lazyScripts.forEach(function (lazyScript) {
+        return processElement(lazyScript);
+      });
+    }
+    /**
+     * check every entry if it is `isIntersecting` and load
+     * its scripts.
+     * when scripts are loaded, the element will be removed from
+     * IntersectionObserver
+     * @param {Array} entries - IntersectionObserver entry array
+     * @param {IntersectionObserver} observer - the IntersectionObserver itself
+     */
+
+
+    function intersectionCallback(entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          processElement(entry.target);
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+    /**
+     * setup IntersectionObserver (if available)
+     * or start loading all scripts within a `requestAnimationFrame`
+     * callback - if available
+     */
+
+
+    function setup() {
+      initLoadedScripts();
+
+      if (!window.IntersectionObserver) {
+        if (!window.requestAnimationFrame) {
+          fallbackScriptLoad();
+        } else {
+          window.requestAnimationFrame(function () {
+            return fallbackScriptLoad();
+          });
+        }
+
+        return;
+      }
+
+      var io = new IntersectionObserver(function (entries, observer) {
+        return intersectionCallback(entries, observer);
+      }, options.intersectionObserverOptions);
+      lazyScripts.forEach(function (lazyScript) {
+        io.observe(lazyScript);
+      });
+    }
+
+    setup();
+  }
+
+  return lazyScripts;
+
+}));
