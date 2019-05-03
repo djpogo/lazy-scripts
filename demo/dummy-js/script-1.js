1fc1fc151e5bc8caec66c5616501a6a004a8d228
Raoul Kramer <raoul.kramer@bold-ventures.de>
2019-05-03 08:35:07 +0200

    update demo page and demo scripts

--- demo/dummy-js/script-1.js
+++ demo/dummy-js/script-1.js
@@ -1,3 +1,4 @@
 (function script1() {
-  console.log("Script 1 loaded, parsed, executed - it's a IIFE");
+  console.log('Script 1 loaded, parsed, executed - it\'s a IIFE');
+  document.querySelector('[data-script-one-target]').textContent = 'Script 1 loaded, parsed and executed…';
 }());
--- demo/dummy-js/script-2.js
+++ demo/dummy-js/script-2.js
@@ -1,3 +1,4 @@
 (function script2() {
   console.log("Script 2 loaded, parsed, executed - it's a IIFE");
+  document.querySelector('[data-script-two-target').textContent = 'Script 2 loaded, parsed and executed… and look moment is here: ' + moment().format('MMMM Do YYYY, h:mm:ss a');
 }());
