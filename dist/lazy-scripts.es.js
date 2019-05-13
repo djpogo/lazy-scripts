cb9bf7a162d23e49cf8c6a7516efb439fc0c1357
Raoul Kramer <raoul.kramer@bold-ventures.de>
2019-05-13 16:40:58 +0200

    0.2.1 release

--- dist/lazy-scripts.es.js
+++ dist/lazy-scripts.es.js
@@ -1,4 +1,4 @@
-/*! LazyScripts - v0.2.0 - 2019-05-13
+/*! LazyScripts - v0.2.1 - 2019-05-13
 * https://lazyscripts.raoulkramer.de
 * Copyright (c) 2019 Raoul Kramer; Licensed GNU General Public License v3.0 */
 
